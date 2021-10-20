65029f36de40381a382f319256e6cb2364ee84dc
Eric Schoenborn <cricketevs@gmail.com>
2021-10-20 16:35:48 -0400

    generic API

---
+++ src/LeftTuesday/Controllers/Debug.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeftTuesday.Controllers
+{
+    [Route("[controller]")]
+    public class Debug : Controller
+    {
+        [HttpGet("ping")]
+        public IActionResult Index()
+        {
+            return Ok("Pong");
+        }
+    }
+}
